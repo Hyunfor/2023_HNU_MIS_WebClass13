38661b9ecfd145c4b7bac8f6063ce5d5f85d62c8
Hyunfor <navel5148@gmail.com>
2023-06-07 15:03:16 +0900

    업로드 테스트 및 서버 설정

---
+++ NonageShopSeqs.sql
@@ -0,0 +1,14 @@
+drop sequence cart_seq;
+drop sequence orders_seq;
+drop sequence order_detail_seq;
+drop sequence product_seq;
+drop sequence qna_seq;
+
+CREATE SEQUENCE cart_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE orders_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE order_detail_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE product_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE qna_seq START WITH 1 INCREMENT BY 1;
+
+
+
